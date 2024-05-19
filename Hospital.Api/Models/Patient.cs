629ae06ee68520fc60f32f53405a3c7aceabf975
bymarium <marianamejia.91619@gmail.com>
2024-05-19 17:04:50 -0500

    Create the entity patient

---
+++ Hospital.Api/Models/Patient.cs
@@ -0,0 +1,9 @@
+﻿namespace Hospital.Api.Models
+{
+  public class Patient : User
+  {
+    public int PatientId { get; set; }
+    public int Age { get; set; }
+    public string Rh {  get; set; }
+  }
+}

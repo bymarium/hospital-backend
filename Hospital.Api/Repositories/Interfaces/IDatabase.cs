7979486929d075ec61f92fcd3e3875113713dc9e
bymarium <marianamejia.91619@gmail.com>
2024-05-19 22:22:03 -0500

    Create the IDatabase interface

---
+++ Hospital.Api/Repositories/Interfaces/IDatabase.cs
@@ -0,0 +1,16 @@
+﻿using Hospital.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Api.Repositories.Interfaces
+{
+  public interface IDatabase
+  {
+    public DbSet<Role> Role {  get; }
+    public DbSet<User> User { get; }
+    public DbSet<Patient> Patient { get; }
+    public DbSet<Doctor> Doctor { get; }
+    public DbSet<Appointment> Appointment { get; }
+
+    Task<bool> SaveAsync();
+  }
+}

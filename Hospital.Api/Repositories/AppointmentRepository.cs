e804f619ddc511b9a8cca0d3ccdff676a5efbe4f
bymarium <marianamejia.91619@gmail.com>
2024-05-19 23:34:57 -0500

    Create the creation method for appointments

---
+++ Hospital.Api/Repositories/AppointmentRepository.cs
@@ -0,0 +1,48 @@
+﻿using Hospital.Api.Models;
+using Hospital.Api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Api.Repositories
+{
+  public class AppointmentRepository : IRepository<Appointment>
+  {
+    private readonly IDatabase _database;
+
+    public AppointmentRepository(IDatabase database)
+    {
+      _database = database;
+    }
+
+    public async Task<Appointment?> CreateAsync(Appointment entity)
+    {
+      _database.Appointment.Add(entity);
+
+      if (!await _database.SaveAsync())
+      {
+        return null;
+      }
+
+      return await _database.Appointment.FirstOrDefaultAsync(appointment => appointment.AppointmentId.Equals(entity.AppointmentId));
+    }
+
+    public Task<bool> DeleteAsync(Appointment entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Appointment>> GetAllAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Appointment?> GetByIdAsync(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(Appointment entity)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

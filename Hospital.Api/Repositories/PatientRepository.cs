128e8f9d0db5c8114287cf73a73c382e270cac57
bymarium <marianamejia.91619@gmail.com>
2024-05-19 23:12:40 -0500

    Create the creation method for patients

---
+++ Hospital.Api/Repositories/PatientRepository.cs
@@ -0,0 +1,50 @@
+﻿using Hospital.Api.Models;
+using Hospital.Api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Api.Repositories
+{
+  public class PatientRepository : IRepository<Patient>
+  {
+    private readonly IDatabase _database;
+
+    public PatientRepository(IDatabase database)
+    {
+      _database = database;
+    }
+
+    public async Task<Patient?> CreateAsync(Patient entity)
+    {
+      _database.Patient.Add(entity);
+
+      if (!await _database.SaveAsync())
+      {
+        return null;
+      }
+
+      return await _database.Patient.FirstOrDefaultAsync(patient => patient.PatientId.Equals(entity.PatientId));
+    }
+
+    public async Task<bool> UpdateAsync(Patient entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(Patient entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Patient>> GetAllAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Patient?> GetByIdAsync(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+   
+  }
+}

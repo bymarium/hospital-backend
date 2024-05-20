ecf89aed884aa87369099dbeb7f6c0dfe2a5206b
bymarium <marianamejia.91619@gmail.com>
2024-05-19 21:19:47 -0500

    Configure the appointment key and properties

---
+++ Hospital.Api/Repositories/Config/Tables/AppointmentConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Hospital.Api.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hospital.Api.Repositories.Config.Tables
+{
+  public class AppointmentConfiguration
+  {
+    public AppointmentConfiguration(EntityTypeBuilder<Appointment> entityBuilder)
+    {
+      entityBuilder.HasKey(appointment => appointment.AppointmentId);
+      entityBuilder.Property(appointment => appointment.Date).IsRequired();
+      entityBuilder.Property(appointment => appointment.Surgery).IsRequired();
+      entityBuilder.Property(appointment => appointment.Diagnostic).IsRequired();
+    }
+  }
+}

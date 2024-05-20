d7e75e07470c441c6c55af8f9eb031d800174964
bymarium <marianamejia.91619@gmail.com>
2024-05-19 22:52:32 -0500

    Create the IRepository interface

---
+++ Hospital.Api/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Hospital.Api.Repositories.Interfaces
+{
+  public interface IRepository<T>
+  {
+    Task<T?> CreateAsync(T entity);
+    Task<bool> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(T entity);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+  }
+}

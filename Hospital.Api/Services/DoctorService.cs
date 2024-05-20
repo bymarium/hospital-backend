ba0f121dec86b7991850d0e02f5ab4c62bd01688
bymarium <marianamejia.91619@gmail.com>
2024-05-20 15:32:45 -0500

    Create the create service for doctor

---
+++ Hospital.Api/Services/DoctorService.cs
@@ -0,0 +1,44 @@
+﻿using Hospital.Api.Models;
+using Hospital.Api.Repositories.Interfaces;
+using Hospital.Api.Services.Interfaces;
+using System.Linq.Expressions;
+
+namespace Hospital.Api.Services
+{
+  public class DoctorService : IService<Doctor>
+  {
+    private readonly IRepository<Doctor> _repository;
+
+    public DoctorService(IRepository<Doctor> repository)
+    {
+      _repository = repository;
+    }
+
+    public async Task<Doctor> CreateAsync(Doctor entity)
+    {
+      var doctor = await _repository.CreateAsync(entity);
+
+      if (doctor == null)
+      {
+        throw new Exception("Patient not found");
+      }
+
+      return doctor;
+    }
+
+    public Task<Doctor> DeleteAsync(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Doctor>> GetAllAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Doctor> UpdateAsync(Doctor entity)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}

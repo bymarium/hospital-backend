e6cb6f25d1c653aaa623d886ca3aa6b842f29fb9
bymarium <marianamejia.91619@gmail.com>
2024-05-20 15:54:52 -0500

    Add services to the program

--- Hospital.Api/Program.cs
+++ Hospital.Api/Program.cs
@@ -2,15 +2,23 @@ using Hospital.Api.Models;
 using Hospital.Api.Repositories;
 using Hospital.Api.Repositories.Config;
 using Hospital.Api.Repositories.Interfaces;
+using Hospital.Api.Services;
+using Hospital.Api.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<Database>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer")));
 builder.Services.AddScoped<IDatabase, Database>();
+
 builder.Services.AddScoped<IRepository<Patient>, PatientRepository>();
 builder.Services.AddScoped<IRepository<Doctor>, DoctorRepository>();
 builder.Services.AddScoped<IRepository<Appointment>, AppointmentRepository>();
+
+builder.Services.AddScoped<IService<Patient>, PatientService>();
+builder.Services.AddScoped<IService<Doctor>, DoctorService>();
+builder.Services.AddScoped<IService<Appointment>, AppointmentService>();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

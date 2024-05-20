ca06b112c089167d85b9f7404481cdee3ae9987a
bymarium <marianamejia.91619@gmail.com>
2024-05-20 15:14:31 -0500

    Create the IService interface

---
+++ Hospital.Api/Services/Interfaces/IService.cs
@@ -0,0 +1,10 @@
+﻿namespace Hospital.Api.Services.Interfaces
+{
+  public interface IService<T>
+  {
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+  }
+}

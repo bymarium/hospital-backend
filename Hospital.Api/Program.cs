72a97a36a67ee2db12be7280fe56ba70de35552c
bymarium <marianamejia.91619@gmail.com>
2024-05-19 22:46:33 -0500

    Configure the database context

--- Hospital.Api/Program.cs
+++ Hospital.Api/Program.cs
@@ -1,5 +1,11 @@
+using Hospital.Api.Repositories.Config;
+using Hospital.Api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<Database>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer")));
+builder.Services.AddScoped<IDatabase, Database>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

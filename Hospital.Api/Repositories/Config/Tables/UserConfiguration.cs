d600aab0dbee43ac7415caa0cc1ec0225b6e0b54
bymarium <marianamejia.91619@gmail.com>
2024-05-19 20:41:42 -0500

    Configure the user key and properties

---
+++ Hospital.Api/Repositories/Config/Tables/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Hospital.Api.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hospital.Api.Repositories.Config.Tables
+{
+  public class UserConfiguration
+  {
+    public UserConfiguration(EntityTypeBuilder<User> entityBuilder)
+    {
+      entityBuilder.HasKey(user => user.UserId);
+      entityBuilder.Property(user => user.Name);
+      entityBuilder.Property(user => user.Email);
+      entityBuilder.Property(user => user.Password);
+    }
+  }
+}

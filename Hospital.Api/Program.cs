1382d20b2e4c7f1bce6547961221a73d00901b45
bymarium <marianamejia.91619@gmail.com>
2024-05-20 15:52:36 -0500

    Add repositories to the program

--- Hospital.Api/Program.cs
+++ Hospital.Api/Program.cs
@@ -1,3 +1,5 @@
+using Hospital.Api.Models;
+using Hospital.Api.Repositories;
 using Hospital.Api.Repositories.Config;
 using Hospital.Api.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<Database>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer")));
 builder.Services.AddScoped<IDatabase, Database>();
+builder.Services.AddScoped<IRepository<Patient>, PatientRepository>();
+builder.Services.AddScoped<IRepository<Doctor>, DoctorRepository>();
+builder.Services.AddScoped<IRepository<Appointment>, AppointmentRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

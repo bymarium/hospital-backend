88874b6e4356f191072a5d6a64aad3f55bd62e53
bymarium <marianamejia.91619@gmail.com>
2024-05-20 23:03:54 -0500

    Create the HttpPost method for the appointment

---
+++ Hospital.Api/Controllers/AppointmentController.cs
@@ -0,0 +1,31 @@
+﻿using Hospital.Api.Dtos;
+using Hospital.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Api.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class AppointmentController : ControllerBase
+  {
+    private readonly IAppointmentService _service;
+
+    public AppointmentController(IAppointmentService service)
+    {
+      _service = service;
+    }
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> Create([FromBody] AppointmentDto appointmentDto)
+    {
+      try
+      {
+        return StatusCode(StatusCodes.Status201Created, await _service.CreateAsync(appointmentDto));
+      }
+      catch (Exception exception)
+      {
+        return StatusCode(StatusCodes.Status404NotFound, new { Error = exception.Message });
+      }
+    }
+  }
+}

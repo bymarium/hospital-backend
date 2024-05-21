e8b9b2cc545d643a8de1db8d30d8e859d693c2d6
bymarium <marianamejia.91619@gmail.com>
2024-05-20 20:45:13 -0500

    Create DTO for patient, doctor and appointment

---
+++ Hospital.Api/Dtos/AppointmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Hospital.Api.Dtos
+{
+  public class AppointmentDto
+  {
+    public DateTime Date { get; set; }
+    public string Surgery { get; set; }
+    public string Diagnostic { get; set; }
+    public int PatientId { get; set; }
+    public int DoctorId { get; set; }
+  }
+}
